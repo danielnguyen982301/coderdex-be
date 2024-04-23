86c357ea625853920eaa73c39eaff026d515d186
danielnguyen982301 <danielnguyen982301@gmail.com>
2024-04-23 22:23:07 +0700

    Modified

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 const pokemonRouter = require("./pokemons.api");
 
 /* GET home page. */
+
+router.get("/", (req, res) => {
+  res.status(200).send("Just a simple Pokemon API");
+});
+
 router.use("/pokemons", pokemonRouter);
 
 router.use("/images", express.static("pokemon"));
